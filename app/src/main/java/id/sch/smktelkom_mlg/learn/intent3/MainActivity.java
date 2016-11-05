cdfe97d4df0f0789d0fc82dfd604e1e41229e759
Detry Ajeng P <detry.ap211298@gmail.com>
2016-11-05 09:54:29 +0700

    Intent Camera (Photo)

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -30,6 +30,31 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openWebPage("http://www.smktelkom-mlg.sch.id/");}});
+
+        findViewById(R.id.imageViewCamera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                capturePhoto();}});
+    }
+
+    static  final int REQUEST_IMAGE_CAPTURE =1;
+    private void capturePhoto()
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK)
+        {
+            Bitmap bitmap = data.getParcelableExtra("data");
+            ImageView iv = (ImageView) findViewById(R.id.imageViewCamera);
+            iv.setImageBitmap(bitmap);
+        }
     }
 
     private void openWebPage(String url)
